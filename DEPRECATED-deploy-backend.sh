3f63d2036a9d6a3bf445c5054d0c718d1246ed23
Santiago Garcia Arango <santiago.garcia1999@hotmail.com>
2023-06-03 00:39:48 -0500

    Update DEPRECATION paths for old features

--- DEPRECATED-deploy-backend.sh
+++ DEPRECATED-deploy-backend.sh
@@ -2,11 +2,11 @@
 set -x
 
 # Prepare Python dependencies for Lambda Function (Layers)
-pip install -r ./src-lambda/requirements-openai.txt --target=./DEPRECATED-layer-openai/python
-pip install -r ./src-lambda/requirements-pandas.txt --target=./DEPRECATED-layer-pandas/python
-pip install -r ./src-lambda/requirements-extras.txt --target=./DEPRECATED-layer-extras/python
+pip install -r ./DEPRECATED-src-lambda/requirements-openai.txt --target=./DEPRECATED-layer-openai/python
+pip install -r ./DEPRECATED-src-lambda/requirements-pandas.txt --target=./DEPRECATED-layer-pandas/python
+pip install -r ./DEPRECATED-src-lambda/requirements-extras.txt --target=./DEPRECATED-layer-extras/python
 
 # Deploy CDK Stack
-cd ./cdk-lambda
+cd ./DEPRECATED-cdk-lambda
 npm install .
 cdk deploy --require-approval never
